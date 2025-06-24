267a80f08c58a85191ae4a751b23006409485a16
uranaveer <uranaveer5238@gmail.com>
2025-06-24 23:28:42 +0530

    Merged some apis into one, removing redundancy

--- server/app/serializer.py
+++ server/app/serializer.py
@@ -1,7 +1,13 @@
-from .models import Post
+from .models import Post,UserData
 from rest_framework import serializers
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserData
+        fields = ['username','avatar_id']
+
 class PostSerializers(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Post
-        fields ='__all__'
+        fields =['id','title','description','link','user']
