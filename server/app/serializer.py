78cd937d632e6b1430de15b44cbde7078711ae56
Manideep <avunurimanideep45@gmail.com>
2025-06-27 03:31:14 +0530

    Remove server and extension 2 folders from tracking

--- server/app/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import Post
-from rest_framework import serializers
-
-class PostSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields ='__all__'
